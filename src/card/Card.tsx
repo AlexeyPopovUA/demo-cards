2981cbe453bf3810441706356ffd13719d2eeb72
Oleksii Popov <alexejpopovua@gmail.com>
2022-10-23 17:12:39 +0200

    feat: style the card

--- src/card/Card.tsx
+++ src/card/Card.tsx
@@ -20,15 +20,16 @@ type Props = {
 
 const Card = (props: Props) => {
     return <div className="card col-4 p-card--highlighted demo-p-card">
-        <header className="p-card__header">
-            <h3>{props.type}</h3>
+        <header className="p-card__header demo-p-card__header">
+            <h5 className="p-muted-heading demo-p-card__type">{props.type}</h5>
+            <hr className="is-muted u-no-margin--bottom" />
         </header>
-        <div className="p-card__content">
+        <div className="p-card__content demo-p-card__content">
             <a className="p-card__image" href={props.link}><img src={props.imageData.src} alt={props.imageData.alt}/></a>
-            <h3 className="p-heading-3"><a href={props.link}>{props.title}</a></h3>
-            <div>By {props.authors.map(author => <a key={author.name} href={author.link}>{author.name}&nbsp;</a>)} on {props.date}</div>
+            <h3 className="demo-p-card__heading"><a href={props.link} className="p-button__link">{props.title}</a></h3>
+            <p><em>By {props.authors.map(author => <a key={author.name} href={author.link}>{author.name}&nbsp;</a>)} on {props.date}</em></p>
         </div>
-        <div className="p-card__footer">
+        <div className="p-card__footer demo-p-card__footer">
             <hr className="is-muted" />
             <div>{props.category}</div>
         </div>
